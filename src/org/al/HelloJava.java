aee3d44e85b1196c738aebb4e1bf3debe479d815
albertleecn <albert_lee@163.com>
2016-04-28 12:18:41 +0800

    Hello Java

---
+++ src/org/al/HelloJava.java
@@ -0,0 +1,20 @@
+package org.al;
+/**
+ * 
+ */
+
+/**
+ * @author allab
+ *
+ */
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Java\n");
+	}
+
+}
